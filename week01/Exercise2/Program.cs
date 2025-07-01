2c0d6fc9a0990a1504ad3cb3a38a9b38f2aa7004
Ethan O'Brien <ethan.a.obrien@gmail.com>
2025-07-01 08:31:09 -0500

    Exercise 1 and 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade number? ");
+        var input = Console.ReadLine();
+        var grade = int.Parse(input);
+
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        var minorValue = grade % 10;
+        var sign = "";
+        if (minorValue >= 7)
+        {
+            sign = "+";
+        }
+        else if (minorValue < 3)
+        {
+            sign = "-";
+        }
+        var gradeSignLetter = $"{letter}{sign}";
+        if (gradeSignLetter == "A+" || letter == "F" || grade >= 100)
+        {
+            gradeSignLetter = letter;
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        var a = letter == "A" || letter == "E" || letter == "F" ? "an" : "a";
+        Console.WriteLine($"You have {a}: {gradeSignLetter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass!");
+        }
+
+        //Console.WriteLine("Hello World! This is the Exercise2 Project.");
     }
 }
