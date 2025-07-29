7f033644b9ae0d9616c69c8db78ae51b4083ff28
Ethan O'Brien <ethan.a.obrien@gmail.com>
2025-07-29 11:45:07 -0500

    week5 homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        var a1 = new Assignment("Billy Bob", "Lunchtime");
+        Console.WriteLine(a1.GetSummary());
+
+        var a2 = new MathAssignment("Steve Jones", "Multiplication", "9.16", "1-20");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        var a3 = new WritingAssignment("Jane Doe", "Personal Narrative", "The moment that changed your life");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
