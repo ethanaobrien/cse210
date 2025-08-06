31cd997b8bf9e1456afd43004282342a4e608fbc
Ethan O'Brien <ethan.a.obrien@gmail.com>
2025-08-06 08:06:29 -0500

    Shape project

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        var shapes = new List<Shape>();
+
+        var shape1 = new Square("Orange", 5);
+        shapes.Add(shape1);
+
+        var shape2 = new Rectangle("Green", 2, 4);
+        shapes.Add(shape2);
+
+        var shape3 = new Circle("Red", 10);
+        shapes.Add(shape3);
+
+        foreach (var shape in shapes)
+        {
+            var color = shape.GetColor();
+            var area = shape.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
